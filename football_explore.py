0eda79e5c3d7b804d601d228678719507b24ad7c
Yusuf Mohamoud <Yusuf.Mohamoud@citystgeorges.ac.uk>
2026-03-15 16:16:25 +0000

    Comments added to the code.

--- football_explore.py
+++ football_explore.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 url = "https://raw.githubusercontent.com/jfjelstul/englishfootball/master/data-csv/matches.csv"
-
 df = pd.read_csv(url)
+# Goes to the URL and reads the CSV file into a DataFrame, df
+# Df: table with rows & columns, every football match is one row
 
-print(df.shape)
-print(df.head())
+print(df.shape) # Dimensions of the dataset table
+print(df.head()) # First 5 rows of the dataset
 
 print("\n--- COLUMN NAMES ---")
 print(df.columns.tolist())
@@ -16,8 +17,14 @@ print(df.isnull().sum())
 print("\n--- BASIC STATS ---")
 print(df.describe())
 
+# New columns for analysis of home and away scores together
+# for every single row simultaneously
 print("\n--- TOP 10 HIGHEST SCORING GAMES ---")
 df['total_goals'] = df['home_team_score'] + df['away_team_score']
+
+# Selects only those 5 columns
+# Sorts by total goals highest to lowest
+# Shows only the top 10 rows of the sorted table
 print(df[['season', 'home_team_name', 'away_team_name', 'score', 'total_goals']]
     .sort_values('total_goals', ascending=False)
     .head(10))
